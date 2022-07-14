872f56c869f1b14bf988a2fa13f766b6e29281ca
vvsivaprasad <sivaprasadsoft@gmail.com>
2022-07-14 11:41:47 +0530

    Add files via upload

---
+++ Prag.java
@@ -0,0 +1,14 @@
+ 
+ 
+public class Prag {
+    
+    public static void main(String args[])
+  {
+      int x=10;
+      int y=50;
+      int sum=x+y;
+      System.out.println("Sum of x and y is: "+sum);
+  } 
+ 
+  
+}
